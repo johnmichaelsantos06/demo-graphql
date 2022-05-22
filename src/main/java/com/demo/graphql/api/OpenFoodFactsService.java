4b445fa90ac05f2df08d93e588e6ee0bde071703
John Michael Santos <johnmichaelsantos06@gmail.com>
2022-05-22 23:12:20 +0800

    Addition of mutation

--- src/main/java/com/demo/graphql/api/OpenFoodFactsService.java
+++ src/main/java/com/demo/graphql/api/OpenFoodFactsService.java
@@ -2,22 +2,33 @@ package com.demo.graphql.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
+import com.demo.model.OpenFoodFactsModel;
 import com.demo.openfoodfacts.api.OpenFoodFactsClient;
 import com.demo.openfoodfacts.response.OpenFoodFactsResponse;
 
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @GraphQLApi
 @Service
+@Validated
 public class OpenFoodFactsService {
 	
 	@Autowired
 	private OpenFoodFactsClient openFoodFactsClient;
 	
 	@GraphQLQuery
-    public OpenFoodFactsResponse productDetails(String id) {
+    public OpenFoodFactsResponse productDetails(@GraphQLNonNull String id) {
         return openFoodFactsClient.getProductDetails(id);
+    }
+	
+	// example usage of mutation only but in reality the method here should be saving to a data store
+	@GraphQLMutation
+    public OpenFoodFactsResponse createProduct(@GraphQLNonNull OpenFoodFactsModel model) {
+		return openFoodFactsClient.getProductDetails(model.getCode());
     }
 }
